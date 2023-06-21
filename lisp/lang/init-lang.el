188cdd8a4f1dfea9f6e4967068f2f7197954b11b
hsun <qiuxunxun@yahoo.com>
2023-06-21 10:50:39 +0800

    Add eglot-format in eglot-mode

--- lisp/lang/init-lang.el
+++ lisp/lang/init-lang.el
@@ -57,28 +57,26 @@
   :hook ((c-mode c++-mode css-mode go-mode java-mode js-mode kotlin-mode python-mode rust-mode ruby-mode web-mode) . eglot-ensure)
   :config
   (add-to-list 'eglot-server-programs '(web-mode "vls"))
-  (defun eglot-format-buffer-on-save ()
-    (add-hook 'before-save-hook #'eglot-format-buffer -10 t))
-  (add-hook 'go-mode-hook #'eglot-format-buffer-on-save)
 
-  (defun +eglot-organize-imports() (call-interactively 'eglot-code-action-organize-imports))
-  (add-hook 'before-save-hook '+eglot-organize-imports nil t)
+  (defun eglot-actions-before-save()
+    (add-hook 'before-save-hook (lambda ()
+                                   (call-interactively #'eglot-format)
+                                   (call-interactively #'eglot-code-action-organize-imports))))
 
-  ;;(advice-add 'eglot-code-action-organize-imports :before #'eglot-format-buffer)
-  ;;(add-hook 'eglot-managed-mode-hook (lambda () (add-hook 'before-save-hook #'eglot-format-buffer))))
-  )
+  (add-hook 'eglot--managed-mode-hook #'eglot-actions-before-save))
 
+
+;; golang indent
 (add-hook 'go-mode-hook
 	    (lambda ()
 	      (setq indent-tabs-mode 1)
 	      (setq tab-width 4)))
 
 
-
-
 ;; vterm is better than builtin eshell
 (use-package vterm
   :bind
+  ;; builtin project.el
   ;; remap the project-shell to use vterm
   ([remap project-shell] . vterm))
 
